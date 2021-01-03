f2be92366099d9f07d6f44b9d0c82ec400889eac
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-03 16:29:31 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,13 +1,13 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count do |element|
-    element.
+    element.include?(String)
   end
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   array.count do |element|
-    element.to_a(String) && element.length == 0 
+    element.include?(String) && element.length == 0 
   end
 end
